bf1ce55b7ef761a832c8998fea7071558108ed68
Daiki Kazusaki <yujiroimanaka0926@gmail.com>
2025-05-03 22:24:48 +0900

    change the limit of timeout

--- app/components/ClientProvider.tsx
+++ app/components/ClientProvider.tsx
@@ -16,7 +16,7 @@ export default function ClientProvider({
       if (timeoutRef.current) clearTimeout(timeoutRef.current);
       timeoutRef.current = setTimeout(() => {
         router.push('/');
-      }, 1 * 60 * 1000); // 10分
+      }, 10 * 60 * 1000); // 10分
     };
 
     const handleVisibilityChange = () => {
